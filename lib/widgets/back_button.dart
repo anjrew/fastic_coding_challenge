4389841c8ce974258b4acb6662af2afd656ce1eb
earyzhe <earyzhe@gmail.com>
2019-09-23 09:35:37 +0200

    Removed Google services.json

--- lib/widgets/back_button.dart
+++ lib/widgets/back_button.dart
@@ -4,15 +4,15 @@ import 'package:nutrition_questionnaire/logic/main_bloc.dart';
 class ArrowBackButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-	return Positioned(
-                left: 10,
-                width: 50,
-                top: 25,
-                height: 50,
-                child: IconButton(
-                    icon: Icon(Icons.arrow_back_ios),
-                    onPressed: MainBloc.of(context).previousQuestion,
-                ),
-            );
+    return Positioned(
+      left: 10,
+      width: 50,
+      top: 25,
+      height: 50,
+      child: IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        onPressed: MainBloc.of(context).previousQuestion,
+      ),
+    );
   }
 }
